c7b6922a2d1c7def328c90a73032dbf6a3c24258
yogvjti <57307256+yogvjti@users.noreply.github.com>
2019-11-07 10:28:21 +0530

    Add files via upload

---
+++ 1_varibles.py
@@ -0,0 +1,31 @@
+print("||Shri Swami Samarth||")
+print("** Welcome to the Yogesh Programming **")
+
+n = 15
+print(n)
+print(type(n))
+print(id(n))
+
+name= "Yogesh Pakhale."
+name1="Yogesh"
+name2="Pakhale"
+print(name1+name2)
+
+print("My name is",name)
+print(type(name))
+print(id(n))
+
+m = 3.14
+print(m)
+print(type(m))
+print(id(m))
+
+a =5
+b =6
+c= complex(a,b)
+print(complex(a,b))
+
+print(True)
+print(type(True))
+
+
